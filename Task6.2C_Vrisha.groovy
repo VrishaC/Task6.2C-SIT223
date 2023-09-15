933a0b631f3529694107a44aac5c89872d5e983b
VrishaC <140986857+VrishaC@users.noreply.github.com>
2023-09-15 21:14:31 +1000

    Update Task6.2C_Vrisha.groovy

--- Task6.2C_Vrisha.groovy
+++ Task6.2C_Vrisha.groovy
@@ -4,95 +4,131 @@ pipeline {
 
     stages {
         stage('Build') {
-            steps{
+            steps {
                 echo "Building..."
             }
-        }
-        post {
+
+            post {
             success {
                 mail to: "vcooblall2021@gmail.com",
+
                 subject: "Build Status Email",
+
                 body: "Build was successful!"
             }
-        }
-        
+            } 
+        }    
+
         stage('Unit and Integration Tests') {
             steps {
-                echo "Running unit and integration tests..."
+                echo "Unit and integration tests..."
             }
+
             post {
                 success {
+
                     mail to: "vcooblall2021@gmail.com",
-                    subject: "Running unit and integration tests Status Email",
-                    body: "Running unit and integration tests was successful!"
-                }
-            }           
-        }
-        
-        stage('Code Analysis') {
-            steps {
-                echo "Analyzing the code..."
-            }
-            post {
-                success {
-                    mail to: "vcooblall2021@gmail.com",
-                    subject: "Code Analysis Status Email",
-                    body: "Code Analysis was successful!"
-                }
-            } 
-        }
-        
-        stage('Security Scan') {
-            steps {
-                echo "Scanning for security vulnerabilities..."
-            }
-            post {
-                success {
-                    mail to: "vcooblall2021@gmail.com",
-                    subject: "Security Scan Status Email",
-                    body: "Security Scan was successful!"
+
+                    subject: "Unit and integration tests Status Email",
+
+                    body: "Unit and integration tests was successful!"
+
                 }
             }          
         }
-        
+
+        stage('Code Analysis') {
+            steps {
+                echo "Code Analysis"
+            }
+
+            post {
+                success {
+
+                    mail to: "vcooblall2021@gmail.com",
+
+                    subject: "Code Analysis Email",
+
+                    body: "Code Analysis was successful!"
+
+                }
+            }
+        }
+
+        stage('Security Scan') {
+
+            steps {
+                echo "Security Scan"
+            }
+
+            post {
+                success {
+
+                    mail to: "vcooblall2021@gmail.com",
+
+                    subject: "Security Scan Status Email",
+
+                    body: "Security Scan was successful!"
+
+                }
+            }          
+        }
+
         stage('Deploy to Staging') {
+
             steps {
-                echo "Deploying to staging..."
+                echo "Deploy to staging..."
             }
+
             post {
                 success {
+
                     mail to: "vcooblall2021@gmail.com",
+
                     subject: "Deploy to Staging Status Email",
+
                     body: "Deploy to Staging was successful!"
+
                 }
-            } 
+            }
         }
-        
+
         stage('Integration Tests on Staging') {
+
             steps {
-                echo "Running integration tests on staging..."
+                echo "Integration tests on staging..."
             }
+
             post {
+
                 success {
+
                     mail to: "vcooblall2021@gmail.com",
-                    subject: "Integration Tests on Staging Status Email",
-                    body: "Integration Tests on Staging was successful!"
+
+                    subject: "Integration Tests Status Email",
+
+                    body: "Integration Tests was successful!"
+
                 }
-            } 
+            }
         }
-        
+
         stage('Deploy to Production') {
+
             steps {
-                echo "Deploying to production..."
+                echo "Deploy to production..."
             }
+
             post {
                 success {
                     mail to: "vcooblall2021@gmail.com",
-                    subject: "Build Status Email",
-                    body: "Build was successful!"
+
+                    subject: "Deploy Status Email",
+
+                    body: "Deploy was successful!"
+
                 }
-            } 
+            }
         }
-    
     }
 }
